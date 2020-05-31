81ec322f3d1032303716c982dec31c102683c6fb
Alex <alexmoberhofer@gmail.com>
2020-05-31 11:24:37 -0400

    package script

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Package Windows"
+
+mkdir ./windows
+
+mv ./sdl2gnuboy.exe ./windows
+
+cp ./README.md ./windows
+
+cd ./windows
+
+wget https://www.libsdl.org/release/SDL2-2.0.12-win32-x64.zip
+
+unzip SDL2-2.0.12-win32-x64.zip 
+
+rm SDL2-2.0.12-win32-x64.zip 
+
+cd ..
+
+zip -r windows-latest.zip windows/
+
+rm -rf windows
+
